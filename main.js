e6828a80fc6e8324d7a7acf03bb26935beb72171
vikeju <virginie.grolleau+github@protonmail.com>
2021-02-22 15:33:48 +0100

    enregistrement cartes

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        // 1er click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // 2e click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // les cartes correspondent-elles?
+        console.log(firstCard.dataset.framework);
+        console.log(secondCard.dataset.framework);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
